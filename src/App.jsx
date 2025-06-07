050532e3583a7e5fd5b37c6ebf42f5a8add63b00
Bibek Bhatta <bhattabbk1@gmail.com>
2025-06-07 16:31:58 +0545

    feat:React Router Layout Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import './App.css'
+import Navbar from './components/Navbar';
+import Signuppage from './pages/Signuppage';
+import Loginpage from './pages/Loginpage';
+import Homepage from './pages/Homepage';
+import Profilepage from './pages/Profilepage';
+import Settingpage from './pages/Settingpage'
+import Footer from './components/Footer';
+import {Routes,Route} from "react-router-dom";
+
 
 function App() {
   
 
   return (
     <>
-    <h1>tailwind</h1>
+  <Navbar/>
+  <Routes>
+    <Route path='/' element={<Homepage/>}/>
+    <Route path='/signup' element={<Signuppage/>}/>
+    <Route path='/login' element={<Loginpage/>}/>
+    <Route path='/setting' element={<Settingpage/>}/>
+    <Route path='/profilepage' element={<Profilepage/>}/>
+    
+  </Routes>
+  <Footer/>
+
     </>
   )
 }
---
+++ src/pages/Profilepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Porfilepage = () => {
+  return (
+    <div>Porfilepage</div>
+  )
+}
+
+export default Porfilepage
