a007d3df1120e1140e83cd25659172627431ba1e
Bibek Bhatta <bhattabbk1@gmail.com>
2025-06-08 13:04:45 +0545

    feat:login part completed with validation and toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css'
+import {useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Signuppage from './pages/Signuppage';
 import Loginpage from './pages/Loginpage';
@@ -8,20 +10,38 @@ import Profilepage from './pages/Profilepage';
 import Settingpage from './pages/Settingpage'
 import Footer from './components/Footer';
 import {Routes,Route} from "react-router-dom";
-
+import { useAuthStore } from './store/useAuthStore';
+import {Loader} from 'lucide-react';
+import {Toaster} from 'react-hot-toast';
 
 function App() {
+  const Navigate = useNavigate();
   
+  const { authUser, isCheckingAuth, checkAuth } = useAuthStore();
+useEffect(()=>{
+  checkAuth()
+},[checkAuth])
+if (isCheckingAuth && !authUser) {
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <Loader className="size-10 animate-spin" />
+    </div>
+  );
+}
+
+
 
   return (
     <>
+      <Toaster position='top-right' reverseOrder={false}/>
   <Navbar/>
   <Routes>
-    <Route path='/' element={<Homepage/>}/>
-    <Route path='/signup' element={<Signuppage/>}/>
-    <Route path='/login' element={<Loginpage/>}/>
+
+    <Route path='/' element={authUser?<Homepage/>:<Navigate to='/login'/>}/>
+    <Route path='/signup' element={!authUser?<Signuppage/>:<Navigate to="/"/>}/>
+    <Route path='/login' element={!authUser?<Loginpage/>:<Navigate to='/'/>}/>
     <Route path='/setting' element={<Settingpage/>}/>
-    <Route path='/profilepage' element={<Profilepage/>}/>
+    <Route path='/profilepage' element={authUser?<Profilepage/>:<Navigate to='/'/>}/>
     
   </Routes>
   <Footer/>
--- src/pages/Signuppage.jsx
+++ src/pages/Signuppage.jsx
@@ -1,9 +1,136 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useAuthStore } from '../store/useAuthStore';
+import ChatImage from "../../public/chat.png";
+import toast from 'react-hot-toast';
+import {Link} from 'react-router-dom';
 
 const Signuppage = () => {
-  return (
-    <div>Signuppage</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    fullname: '',
+    email: '',
+    password: '',
+  });
 
-export default Signuppage
+  const { signup } = useAuthStore();
+
+  const validateForm = () => {
+    if (!formData.fullname.trim()) {
+      toast.error("The Fullname is required");
+      return false;
+    }
+    if (!formData.email.trim()) {
+      toast.error("Email Address is required");
+      return false;
+    }
+    if (!formData.password) {
+      toast.error("Password is required");
+      return false;
+    }
+    if (formData.password.length < 6) {
+      toast.error("Password must be at least 6 characters");
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isValid = validateForm();
+    if (!isValid) return;
+
+    signup(formData);
+    toast.success("Signup successful!");
+  };
+
+  return (
+    <>
+      <div className="min-h-screen grid lg:grid-cols-2">
+        {/* Left column: Signup form */}
+        <div className="flex flex-col justify-center items-center px-6 sm:px-12 py-12 bg-white">
+          <div className="w-full max-w-md space-y-6">
+            <h1 className="text-2xl font-bold text-center">KURAKANI.com</h1>
+
+            <form onSubmit={handleSubmit} className="space-y-5">
+              {/* Full Name */}
+              <label className="input input-bordered flex items-center gap-2">
+                <input
+                  type="text"
+                  name="fullname"
+                  required
+                  placeholder="Username"
+                  value={formData.fullname}
+                  onChange={(e) =>
+                    setFormData({ ...formData, fullname: e.target.value })
+                  }
+                />
+              </label>
+
+              {/* Email */}
+              <label className="input input-bordered flex items-center gap-2">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="mail@site.com"
+                  required
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                />
+              </label>
+
+              {/* Password */}
+              <label className="input input-bordered flex items-center gap-2">
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
+                />
+              </label>
+
+              <div className="space-y-3 pt-4">
+                <button className="btn btn-primary w-full" type="submit">
+                  Sign Up
+                </button>
+
+                <button
+                  type="button"
+                  className="btn w-full bg-white text-black border border-gray-300"
+                >
+                  Sign up with Email
+                </button>
+
+                <button
+                  type="button"
+                  className="btn w-full bg-white text-black border border-gray-300"
+                >
+                  Sign up with Google
+                </button>
+              </div>
+            </form>
+            <div  className='text-center'>
+              <p className='text-base-content/60'>
+              Already have an account?
+              <Link to="/login" className="link link-primary">
+              Sign In
+              </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+
+      
+        <div className="hidden lg:flex items-center justify-center bg-gray-100">
+          <img src={ChatImage} alt="chat-image" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signuppage;
